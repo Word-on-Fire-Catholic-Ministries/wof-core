f9d630916d2d27551e6a1dc6072274004611d3be
Sean Davis <sdavis@wordonfire.org>
2020-12-11 15:17:04 -0600

    Initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Word on Fire Core
+A set of shared libraires and functionality for all Word on Fire WordPress Sites.
---
+++ wof-core.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Word on Fire Core
+ *
+ * @package           WOF\Core
+ * @author            Sean Davis
+ * @copyright         2020 Word on Fire
+ * @license           Proprietary
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Word on Fire Core
+ * Plugin URI:        https://github.com/Word-on-Fire-Catholic-Ministries/wof-core
+ * Description:       A set of shared libraries and functionality for all Word on Fire WordPress Sites.
+ * Version:           1.0.0
+ * Requires at least: 5.5
+ * Requires PHP:      7.4
+ * Author:            Sean Davis
+ * Author URI:        https://github.com/Word-on-Fire-Catholic-Ministries/
+ * Text Domain:       wof-core
+ * License:           None
+ */
