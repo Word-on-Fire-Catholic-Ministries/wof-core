f4946d719d7c0e914497048edcaf541aca69204d
Sean Davis <sdavis@wordonfire.org>
2021-03-09 16:14:19 -0600

    Added h cat tests

--- test/php-tests/Search/IndexerTest.php
+++ test/php-tests/Search/IndexerTest.php
@@ -24,16 +24,40 @@ class IndexerTest extends TestCase
 		$this->assertEquals(7 , sizeof($serialized));
 	}
 
-    public function testSerializeHierarchicalCategoriesReturnsArrayInOrder() {
+    public function testSerializeCategoriesReturnsArrayInOrder() {
 	    $categories = Wofi_Term::make_array(Wofi_Term::test_data());
 	    $pi = new PostIndexer();
 	    $serialized = $pi->serialize_categories($categories);
+	    $this->assertEquals(['read', 'watch', 'articles', 'shows', 'daily gospel reflections', 'word on fire show', 'sermons'], $serialized);
+    }
+
+    public function testSerializeHierarchicalCategoriesReturnsArray() {
+	    $categories = Wofi_Term::make_array(Wofi_Term::test_data());
+	    $pi = new PostIndexer();
+	    $serialized = $pi->serialize_hierarchical_categories($categories);
 	    $this->assertIsArray($serialized);
-	    $this->assertEquals(['look', 'watch', 'articles', 'shows', 'daily gospel reflections', 'word on fire show', 'sermons'], $serialized);
     }
 
-    public function testSerializeHierarchicalCategoriesReturnsCorrectFormat() {
+	public function testSerializeHierarchicalCategoriesReturnsCorrectLength() {
+		$categories = Wofi_Term::make_array(Wofi_Term::test_data());
+		$pi = new PostIndexer();
+		$serialized = $pi->serialize_hierarchical_categories($categories);
+		$this->assertEquals(3 , sizeof($serialized));
+	}
 
-    }
+	public function testSerializeHierarchicalCategoriesReturnsArrayInOrder() {
+		$categories = Wofi_Term::make_array(Wofi_Term::test_data());
+		$pi = new PostIndexer();
+		$serialized = $pi->serialize_categories($categories);
+		$this->assertEquals([
+			'lvl0' => ['read', 'watch'],
+			'lvl1' => ['articles > read', 'shows > watch'],
+			'lvl2' => [
+				'daily gospel reflections > articles > read',
+				'word on fire show > shows > watch',
+				'sermons > shows > watch'
+				]
+			], $serialized);
+	}
 }
 
