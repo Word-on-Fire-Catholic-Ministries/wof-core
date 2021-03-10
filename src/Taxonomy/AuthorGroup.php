04382d70719055c23a18de57f6ddcb7df46ffc21
Sean Davis <sdavis@wordonfire.org>
2021-03-10 09:41:09 -0600

    Added author groups

--- src/Taxonomy/AuthorGroup.php
+++ src/Taxonomy/AuthorGroup.php
@@ -4,6 +4,34 @@
 namespace WOF\Taxonomy;
 
 
+use WOF\Core\Exceptions\WofException;
+use WP_Term;
+
+defined( 'ABSPATH' ) || exit;
+
 class AuthorGroup {
+
+	private WP_Term $term;
+
+	private static string $taxonomy = 'author_groups';
+
+	public function __construct(WP_Term $term) {
+		$this->term = $term;
+	}
+
+	public static function create_from_term(WP_Term $term) {
+		if ($term->taxonomy !== self::$taxonomy) {
+			return false;
+		}
+		return new AuthorGroup($term);
+	}
+
+	public function get_users (): array {
+		return get_objects_in_term($this->term->term_id, self::$taxonomy);
+	}
+
+	public function get_name (): string {
+		return $this->term->name;
+	}
 
 }
